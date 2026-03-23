3aa7d04cfd42df1e70e5b8827ee08a8ccc03b4de
kam1806 <KAMALINI1806@GMAIL.COM>
2026-03-23 19:00:50 +0000

    fixed init

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,4 @@
 from .user import User
 # If you have a note model later, you'll add: from .note import Note
 from .note import Note,QuizQuestion# Good practice to keep Base here too
-from database import Base # Good practice to keep Base here too
+from services.database import Base # Good practice to keep Base here too
